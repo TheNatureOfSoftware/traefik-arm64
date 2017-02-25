f0a3cc1fb9b28ec0bcbed2c8c7f01d2ab8ef40e2
Lars Mogren <lars.mogren@gmail.com>
2017-02-25 23:40:14 +0100

    wget is now quiet.

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ QEMU_VERSION=v2.8.0
 TRAEFIK_VERSION=v1.1.2
 
 if [ ! -f qemu-aarch64-static ]; then
-  wget https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-aarch64-static
+  wget -q https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-aarch64-static
   chmod a+x qemu-aarch64-static
 fi
 
 if [ ! -f traefik_linux-arm64 ]; then
-  wget https://github.com/containous/traefik/releases/download/$TRAEFIK_VERSION/traefik_linux-arm64
+  wget -q https://github.com/containous/traefik/releases/download/$TRAEFIK_VERSION/traefik_linux-arm64
   chmod a+x traefik_linux-arm64
 fi
 
